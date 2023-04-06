eae10b133cb5a08b5bd2571b1bb2ddde92984ecd
ann-moko <129892885+ann-moko@users.noreply.github.com>
2023-04-06 14:08:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,10 @@
 import pandas as pd 
 import numpy as np 
-from scipy.stats import t, expon
-from scipy.stats import exponnorm
 
 chat_id = 371784753 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    a = 2 * x / (71 ** 2)
     alpha = 1 - p
-    loc = a.mean()
-    scale = np.sqrt(np.var(a)) / np.sqrt(len(a))
-    return loc - scale * norm.ppf(1 - alpha / 2), loc - scale * norm.ppf(alpha / 2)
+    left = (-min(-x) - 1 / 2) / (71**2 / 2)
+    right = (-np.log(alpha) / len(x)-min(-x)-1/2) / (71**2/2)
+    return left, right
