a770a2c5860a481c45e0fda34494f6a7169dca38
ann-moko <129892885+ann-moko@users.noreply.github.com>
2023-04-06 12:13:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,16 +2,11 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
-from scipy.stats import t
 
 chat_id = 371784753 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
     alpha = 1 - p
-    df = n - 1
-    t_value = t(df).ppf(1 - alpha / 2)
-    x_mean = np.mean(x)
-    x_std = np.std(x, ddof=1)
-    interval_half_width = t_value * x_std / np.sqrt(n)
-    return x_mean - interval_half_width, x_mean + interval_half_width
+    left = (-min(-x)-1/2) / (71**2/2)
+    right = (-np.log(alpha) / len(x)-min(-x)-1/2) / (71**2/2)
+    return left, right
