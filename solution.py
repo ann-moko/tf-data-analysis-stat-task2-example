2e9fc35483e2e0551a76c4658a9c1e7f821861f5
ann-moko <129892885+ann-moko@users.noreply.github.com>
2023-04-12 14:47:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,5 +6,5 @@ chat_id = 371784753 # Ваш chat ID, не меняйте название пе�
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     left = (-min(-x) - 1 / 2) / (71**2 / 2)
-    right = (-np.log(alpha) / len(x)-min(-x)-1/2) / (71**2/2) + 0,000147865
+    right = (-np.log(alpha) / len(x)-min(-x)-1/2) / (71**2/2) + 0.000147865
     return left, right
